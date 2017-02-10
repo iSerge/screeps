0c616ace3a46c46b0c2f2786d694d6a34aca506c
Serge Ivanov <lutvuk@gmail.com>
2017-02-10 21:36:49 +0300

    Starting code from tutorial 5

---
+++ sync/main.js
@@ -0,0 +1,34 @@
+const roleHarvester = require('./role.harvester');
+const roleUpgrader = require('./role.upgrader');
+const roleBuilder = require('./role.builder');
+
+module.exports.loop = function () {
+
+    const tower = Game.getObjectById('TOWER_ID');
+    if(tower) {
+        const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(let name in Game.creeps) {
+        const creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+};
