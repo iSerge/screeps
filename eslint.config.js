105f400fcb58861233a2203afc2635ad631fb491
Serge Ivanov <lutvuk@gmail.com>
2024-02-23 12:08:48 +0300

    Move to screepers/screeps-typescript-starter build configuration

--- eslint.config.js
+++
@@ -1,7 +0,0 @@
-import { rules, packages } from '@eslint-stylistic/metadata';
-import stylistic from '@stylistic/eslint-plugin';
-console.log(packages);
-export default [
-  stylistic.configs['recommended-flat'],
-  // ...your other config items
-];
